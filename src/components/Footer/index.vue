eeb6347858949a172c156815929ec5d17c9856a6
tolya-yanot <noreply@ton.org>
2025-04-15 20:38:35 +0400

    v3.0.23 - build

--- src/components/Footer/index.vue
+++ src/components/Footer/index.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="Footer">
     <!-- Version -->
-    v3.0.22,
+    v3.0.23,
 
     <!-- Instruction -->
     <a :href="instructionLink" target="_blank">{{$t(`instruction`)}}</a>,
